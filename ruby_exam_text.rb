24f6094e0008d8b03f5283f11ddb3d62b193ca03
面川泰明 <omokawa@senk-inc.co.jp>
2021-09-24 20:33:58 +0900

    20-21

--- ruby_exam_text.rb
+++ ruby_exam_text.rb
@@ -259,3 +259,27 @@ p Greeting # warning and puts 'Hi Ruby'
 
 # q20
 p File.join("ruby", "exam","silver") # ruby/exam/silver
+
+# q21
+class Surface
+  attr_reader :s
+  def initialize(x,y)
+   @s = x * y
+  end
+end
+
+class Volume < Surface
+  attr_reader :v
+  def initialize(x,y,z)
+    super(x,y)
+   @v = x * y * z
+  end
+end
+
+a = Volume.new(2,5,5)
+puts "#{a.v},#{a.s}" # 50,10
+
+#q21
+string = "test code"
+string.slice(0,4)
+p string #test code
