9b2adec2bd94106f58c90313f62d530135e08c01
面川泰明 <omokawa@senk-inc.co.jp>
2021-10-11 16:49:23 +0900

    done

--- ruby_silver_text_exercise.rb
+++ ruby_silver_text_exercise.rb
@@ -203,4 +203,55 @@ File.open('data') do |io|
   end
 end
 
+#29
+t = Time.gm(1970,1,1)
+puts t.strftime("%Y/%m/%d") #1970/01/01
+
+#30
+'New'.match(%r!^[Nn]ew$!) #New
+'New'.match(%r!^[Nn][e][w]$!) #New
+
+#m1
+while
+class
+
+#m2
+x = 0
+[1,2,3].each do |x|
+  print x.to_s + " "
+end
+puts x
+#1 2 3 0
+
+#3
+nil || 'hoge'
+false || 'fuga'
+
+#4
+y = false
+y && (raise 'falied')
+puts ('succeeded!')
+
+#5
+(1..10).each do |i| puts i end #1~10
+(1...10).each do |i| puts i end #1~9
+
+#6
+HOGE = 'hoge'
+HOGE.gsub!('hoge', 'piyo') #NO warning
+print HOGE #piyo
+
+#8
+begin
+  puts 10/0
+rescue ZeroDivisionError => ex
+  print 'ZeroDivisionException:', ex.message
+end
+
+#9
+p ?A #A
+
+#10
+class Hoge < Object; end #ok
+class Fuga < Kernel; end #error
 
