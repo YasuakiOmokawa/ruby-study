163d36c3b8c7636a131516ebc2659ec796c3627b
面川泰明 <omokawa@senk-inc.co.jp>
2021-11-21 15:25:40 +0900

    トップレベルのグローバル変数を削除

--- redflag.rb
+++ redflag.rb
@@ -1,20 +1,34 @@
-def setup(&block)
-  @setups << block
-end
+lambda {
+  events = []
+  setups = []
 
-def event(description, &block)
-  @events << {
-    :description => description,
-    :condition => block
-  }
-end
+  Kernel.send :define_method, :setup do |&block|
+    setups << block
+  end
 
-@setups = []
-@events = []
+  Kernel.send :define_method, :event do |description, &block|
+    events << {
+      description: description,
+      condition: block
+    }
+  end
+
+  Kernel.send :define_method, :each_setup do |&block|
+    setups.each do |setup|
+      block.call setup
+    end
+  end
+
+  Kernel.send :define_method, :each_event do |&block|
+    events.each do |event|
+      block.call event
+    end
+  end
+}.call
 load 'events.rb'
 
-@events.each do |event|
-  @setups.each do |setup|
+each_event do |event|
+  each_setup do |setup|
     setup.call
   end
   puts "ALERT: #{event[:description]}" if event[:condition].call
