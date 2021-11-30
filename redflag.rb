a018346f72ee6b95d9bcdf31e0963d46632049b0
面川泰明 <omokawa@senk-inc.co.jp>
2021-11-30 21:35:44 +0900

    done

--- redflag.rb
+++ redflag.rb
@@ -144,7 +144,23 @@ class C
   def a_method
     'C#a_method'
   end
+  class << self
+    def a_class_method
+      'C.a_class_method()'
+    end
+  end
 end
 class D < C;end
 obj = D.new
 p obj.a_method #C#a_method
+class << obj
+  def a_singleton_method
+    'obj#a_singleton_method()'
+  end
+end
+p obj.singleton_class.superclass #D
+p C.singleton_class ##C
+p D.singleton_class ##D
+p D.singleton_class.superclass ##C
+p C.singleton_class.superclass ##Object
+p D.a_class_method #C.a_class_method()
