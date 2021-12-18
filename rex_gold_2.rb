f563113d680f388df7412f52539be7dc9c5bd21c
面川泰明 <omokawa@senk-inc.co.jp>
2021-12-18 14:41:21 +0900

    done

--- rex_gold_2.rb
+++ rex_gold_2.rb
@@ -117,17 +117,19 @@ module M1
   end
 
   class C2 < C1
-    CONST = "010"
+    # CONST = "010"
+    p CONST
 
     module M2
-      CONST = "011"
+      # CONST = "011"
 
       class Ca
-        CONST = "100"
+        # CONST = "100"
       end
 
       class Cb < Ca
-        p CONST
+        # p const_get(:CONST)
+        # p CONST
       end
     end
   end
@@ -150,3 +152,182 @@ hoge(1,2,3,4) do |args|
   p args.length < 0 ? "hello" : args
 end
 
+
+def m1(*)
+  str = yield if block_given?
+  p "m1 #{str}"
+end
+
+def m2(*args)
+  str = yield(args) if block_given?
+  p "m2 #{str}"
+end
+
+m1 m2 {
+  "hello"
+}
+
+m2 {|args| "hello #{args}"}
+m2(1,2,3,4) {|*args| "hello #{args}"}
+
+
+module M
+  def method_missing(id, *args)
+    puts "M#method_missing"
+  end
+  # class << self
+  #   def method_missing(id, *args)
+  #     puts "M.method_missing"
+  #   end
+  # end
+end
+class A
+  # include M
+  def method_missing(id, *args)
+    puts "A#method_missing"
+  end
+  class << self
+    include M
+    # def method_missing(id, *args)
+    #   puts "A.method_missing"
+    #   super
+    # end
+  end
+end
+class B < A
+  class << self
+    def method_missing(id, *args)
+      puts "B.method_missing"
+      super
+    end
+  end
+end
+
+B.new.dummy_method
+A.new.dummy
+M.dummy
+B.dummy_method
+A.dummy
+
+
+class C
+  def initialize
+  end
+end
+
+p C.new.public_methods.include? :initialize #false
+p C.new.private_methods.include? :initialize #true
+
+
+class Human
+  NAME = "Unknown"
+
+  def self.name
+    const_get(:NAME)
+  end
+  def name
+    methods
+  end
+end
+
+class Fukuzawa < Human
+  NAME = "Yukichi"
+end
+class Natsume < Fukuzawa
+  # NAME = "Yukichi"
+end
+
+puts Fukuzawa.name #Y
+puts Human.new.name #U
+puts Human.name #U
+puts Natsume.name #Y
+puts Human.const_get(:NAME)
+
+
+class C
+end
+
+module M
+  refine C do
+    def m1
+      100
+    end
+  end
+end
+
+class C
+  def m1
+    400
+  end
+
+  def self.using_m
+    using M
+  end
+end
+
+C.using_m
+
+puts C.new.m1
+
+
+module M
+  def refer_const
+    CONST
+  end
+end
+
+module E
+  CONST = '010'
+end
+
+class D
+  CONST = "001"
+end
+
+class C < D
+  include E
+  include M
+  CONST = '100'
+end
+
+c = C.new
+p c.refer_const
+
+
+a = (1..5).partition(&:odd?)
+p a
+
+
+class S
+  @@val = 0
+  def initialize
+    @@val += 1
+  end
+end
+
+class C < S
+  class << C
+    @@val += 2
+  end
+
+  def initialize
+    @@val += 1
+    super
+  end
+end
+
+C.new
+C.new
+S.new
+S.new
+
+p C.class_variable_get(:@@val)
+
+
+def foo(arg2: 1, arg1: 10)
+  p arg1
+  p arg2
+end
+option = {arg2: 900}
+p foo option
+foo **option
