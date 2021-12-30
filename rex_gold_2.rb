68af6aaeed91e971fa49413195a810443d937402
面川泰明 <omokawa@senk-inc.co.jp>
2021-12-30 21:28:30 +0900

    done

--- rex_gold_2.rb
+++ rex_gold_2.rb
@@ -1855,3 +1855,28 @@ puts Sample.instance_variables # => @y
 puts Sample.class_variables # => @@a
 puts sample.output # => 1
 puts Sample.instance_methods.grep(/output/) # => output
+
+
+class S
+  @@val = 0
+  def initialize
+    @@val += 1
+  end
+end
+
+class C < S
+  class << C
+    @@val += 1
+  end
+
+  def initialize
+  end
+end
+
+C.new
+C.new
+S.new
+S.new
+
+p C.class_variable_get(:@@val)
+
