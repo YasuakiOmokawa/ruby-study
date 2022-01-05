6b04723b24b5c769b48a9893a06c3d824329723b
面川泰明 <omokawa@senk-inc.co.jp>
2022-01-05 15:26:10 +0900

    done

--- rex_gold_2.rb
+++ rex_gold_2.rb
@@ -2558,3 +2558,20 @@ module M
   end
 end
 
+
+m = Module.new
+
+m.instance_eval do
+  m.instance_variable_set :@block, Module.nesting
+end
+
+m.instance_eval(<<-EVAL)
+  m.instance_variable_set :@eval,  Module.nesting
+EVAL
+
+block = m.instance_variable_get :@block
+_eval = m.instance_variable_get :@eval
+
+puts block.size
+puts _eval.size
+
