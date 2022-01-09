1607262f90449e67bc74f82e7022836a9c9e3c18
面川泰明 <omokawa@senk-inc.co.jp>
2022-01-09 15:18:43 +0900

    done

---
+++ gold_4.rb
@@ -0,0 +1,18 @@
+class C
+  def self.m1
+    'C.m1'
+  end
+end
+
+module M
+  refine C.singleton_class do
+    def m1
+      'C.m1 in M'
+    end
+  end
+end
+
+using M
+
+puts C.m1 # C.m1 in M と表示されます。
+
