79c517f418cf8e004067d169b03a4e02016b59bb
面川泰明 <omokawa@senk-inc.co.jp>
2022-01-25 09:25:43 +0900

    done

--- gold_5.rb
+++ gold_5.rb
@@ -237,3 +237,26 @@ $num = 0
   load './require.rb'
 end
 puts $num
+
+
+m = Module.new
+
+CONST = "Constant in Toplevel"
+
+_proc = Proc.new do
+  CONST = "Constant in Proc"
+end
+
+m.instance_eval(<<-EOS)
+  CONST = "Constant in Module instance"
+
+  def const
+    CONST
+  end
+EOS
+
+m.module_eval(&_proc)
+
+p m.const
+
+
